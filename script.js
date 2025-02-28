90c0ca4a37b75e265df331930e124d61de2cbe3c
Pzydethuongkkkk <phamhongthai24120723@gmail.com>
2025-02-28 14:02:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u = username.value
   let p = password.value
-  if (u=="thai" && p="123")
+  if (u== "thai" && p= "123" )
   {
     window.open("home.html")
     document.getElementById(compiler).style.display="block"
